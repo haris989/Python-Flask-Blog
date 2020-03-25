f7c2f856acc9f3018d8718634ffdf03a714fff10
Abhishek Pratap Singh <59140024+av1shek@users.noreply.github.com>
2020-03-25 17:56:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from flask_mail import Mail
 import json
 import os
